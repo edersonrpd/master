4b1617980e8f444ced30b62912d19b10dd6574ec
Ederson <edersonrp@gmail.com>
2022-05-23 12:31:32 -0300

    correção listagem arquivos

--- masterformula.py
+++ masterformula.py
@@ -1,12 +1,11 @@
 import os
 
-arquivos = os.listdir()
+diretorio = os.listdir()
+arquivos = list()
 
-excecao = ['masterformula.py', '.git', '.gitignore', 'resultados.txt', 'pedidos', 'master.bat' ]
-
-for exc in excecao:
-    if exc in excecao:
-        arquivos.remove(exc)
+for arquivo in diretorio:
+  if arquivo.endswith('.ped'):
+    arquivos.append(arquivo)
 
 
 def master_formula(file: str):
@@ -34,8 +33,10 @@ def master_formula(file: str):
                 cnpj = linha[8:22]
                 cod_cli = linha[23:31]
                 pedido = linha[31:40]
-                data = linha[42:48]
-
+                data = linha[40:48]
+                dia = data[6:8]
+                mes = data[4:6]
+                ano = data[:4]
             if linha.startswith('09'):
                 qtde_itens = int(linha[3:6])
                 qtde_total_unid = int(linha[7:15])
@@ -43,11 +44,11 @@ def master_formula(file: str):
             linha = arquivo.readline()
 
         print('-' * 48)
-        print(f'Pedido {pedido} Arquivo: {file}')
+        print(f'|Pedido {pedido}  Arquivo: {file}     |')
         print('-' * 48)
         print(f'Identificador = {identificador} Layout = {layout}')
-        print(f'Código Cliente = {cod_cli} CNPJ = {cnpj}')
-        print(f'Data = {data}')
+        print(f'Cod. Cliente = {cod_cli} CNPJ = {cnpj}')
+        print(f'Data = {dia}/{mes}/{ano}')
         print('')
         for produto in produtos:
             print(f'EAN = {produto}')
